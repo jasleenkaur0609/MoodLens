4397f8e1fbaf864f3e578381c6eddccd7d61f687
Jasleen Kaur <jasleensejal2003@gmail.com>
2025-11-19 21:51:16 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,13 +3,13 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAao3A5qY2CfJfSpjJq3VK8pXVgP0HAFCo",
-  authDomain: "moodlens-188a5.firebaseapp.com",
-  projectId: "moodlens-188a5",
-  storageBucket: "moodlens-188a5.firebasestorage.app",
-  messagingSenderId: "175504126348",
-  appId: "1:175504126348:web:af68e5600665ff99c5a6a9",
-  measurementId: "G-7H1HRGEM9T"
+  apiKey: "your-api-key",
+  authDomain: "your-domain",
+  projectId: "your-project-ID",
+  storageBucket: "your-storage-bucket",
+  messagingSenderId: "your-sender-ID",
+  appId: "your-app-id",
+  measurementId: "your-ID"
 };
 
 const app = initializeApp(firebaseConfig);
